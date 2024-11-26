078d0989606ee7866156e690bbaad20d9450132a
Rahul Saini <111075975+Rahul-K-Saini@users.noreply.github.com>
2024-11-26 17:04:53 +0530

    Update Programming.jsx

--- src/components/Programming/Programming.jsx
+++ src/components/Programming/Programming.jsx
@@ -44,7 +44,7 @@ function Programming() {
                 setCode('#include <iostream>\nusing namespace std;\n\nint main() {\n    // Your C++ code here\n    return 0;\n}');
                 break;
             case 'py':
-                setCode('# Your free instance will spin down with inactivity, which can delay requests/n by 50 seconds or more -render\n # Due to this initial request MAY take time');
+                setCode('# Your free instance will spin down with inactivity, which can delay requests by 50 seconds or more -render\n # Due to this initial request MAY take time');
                 break;
             case 'java':
                 setCode('//public class should be CodeCraft \npublic class CodeCraft {\n    public static void main(String[] args) {\n        // Your Java code here\n    }\n}');
